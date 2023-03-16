2d958cce2400afd753c3073cdf311b23dd155b30
Vitalii <lennab@mail.ru>
2023-03-16 19:27:35 +0300

    Исправления сделаны

--- src/main/java/hw/calculator/Homework_2_4/CalculateController.java
+++ src/main/java/hw/calculator/Homework_2_4/CalculateController.java
@@ -14,27 +14,29 @@ public class CalculateController {
 
     @GetMapping
     public String welcome() {
-        return calculateService.welcome();
+        return "<b>Добро пожаловать в калькулятор.</b>";
     }
 
     @GetMapping(path = "/plus" )
     public String plus(@RequestParam("num_1") int num1,@RequestParam("num_2") int num2) {
-        return calculateService.plus(num1, num2);
+        return num1 + " + " + num2 + " = " + calculateService.plus(num1, num2);
     }
+
     @GetMapping(path = "/minus" )
     public String minus(@RequestParam("num_1") int num1,@RequestParam("num_2") int num2) {
-        return calculateService.minus(num1, num2);
+        return num1 + " - " + num2 + " = " + calculateService.minus(num1, num2);
     }
+
     @GetMapping(path = "/multiply" )
     public String multiply(@RequestParam("num_1") int num1,@RequestParam("num_2") int num2) {
-        return calculateService.multiply(num1, num2);
+        return num1 + " * " + num2 + " = " + calculateService.multiply(num1, num2);
     }
+
     @GetMapping(path = "/divide" )
     public String divide(@RequestParam("num_1") int num1,@RequestParam("num_2") int num2) {
-        return calculateService.divide(num1, num2);
-    }
-    @GetMapping(path = "/allComputations" )
-    public String allCalculation(@RequestParam("num_1") int num1,@RequestParam("num_2") int num2) {
-        return calculateService.allCalculation(num1, num2);
+        if (num2 == 0)
+            return "На 0 делить нельзя!";
+        else
+            return num1 + " / " + num2 + " = " + calculateService.divide(num1, num2);
     }
 }
--- src/main/java/hw/calculator/Homework_2_4/CalculateService.java
+++ src/main/java/hw/calculator/Homework_2_4/CalculateService.java
@@ -4,27 +4,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculateService {
-    public String welcome() {
-        return "<b>Добро пожаловать в калькулятор.</b>";
+
+    public int plus(int num1, int num2) {
+        return num1 + num2;
     }
 
-    public String plus(int num1, int num2) {
-        return num1 + " + " + num2 + " = " + (num1 + num2);
-    }
-    public String minus(int num1, int num2) {
-        return num1 + " - " + num2 + " = " + (num1 - num2);
-    }
-    public String multiply(int num1, int num2) {
-        return num1 + " * " + num2 + " = " + (num1 * num2);
-    }
-    public String divide(int num1, int num2) {
-        if (num2 == 0)
-            return "На 0 делить нельзя!";
-        else
-            return num1 + " / " + num2 + " = " + ((float)num1 / num2);
-    }
-    public String allCalculation(int num1, int num2) {
-        return plus(num1, num2) + "<br>" + minus(num1, num2) + "<br>" + multiply(num1, num2) + "<br>" + divide(num1, num2);
+    public int minus(int num1, int num2) {
+        return num1 - num2;
     }
 
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public float divide(int num1, int num2) {
+        return (float)num1 / num2;
+    }
 }
